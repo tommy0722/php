5930c5a001a2625d79dd065c597eccb2d465babc
tommy722 <t40201@gmail.com>
2021-10-22 15:59:37 +0800

    使用迴圈完成天干地支分配

--- array/arraytext5.php
+++ array/arraytext5.php
@@ -4,11 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>已知西元1024年為甲子年，請設計一支程式，可以接受任一西元年份，輸出對應的天干地支的年別。</title>
+    <title>天干地支年。</title>
 </head>
 <body>
+    <h1>天干地支年</h1>
     <?php
-    
+    // 1984 甲子
+    $sky=['甲','乙','丙','丁','戊','己','庚','辛','壬','癸']  ;
+    $land=['子','丑','寅','卯','辰','巳','午','未','申','酉','戌','亥'];
+    $zodiac=[];
+    for($i=0;$i<60;$i++){
+        $zodiac[]=$sky[$i%10].$land[$i%12];
+    }
+    echo "<pre>";
+    print_r($zodiac);
+    echo "</pre>";
+    $tt=[];
+    for($i=0;$i<60;$i++){
+        $tt[1984+$i]=$zodiac[$i];
+    }
+    echo "<pre>";
+    print_r($tt);
+    echo "</pre>";
     ?>
 </body>
 </html>
