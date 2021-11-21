3d11be2af2dce8d596d7e0ee6be9c67f2738f492
tommy0722 <t40201@gmail.com>
2021-11-21 19:01:07 +0800

    萬年曆end01

--- PHP1/HW/calendar/calendar.php
+++ PHP1/HW/calendar/calendar.php
@@ -1,12 +1,7 @@
 <?php
 
+use function PHPSTORM_META\type;
 
-
-// echo "$years <br>";
-// echo "$month<br>";
-// echo "$days<br>";
-// echo "$firstday<br>";
-// echo "$week<br>";
 $weekend = ["日", "一", "二", "三", "四", "五", "六"];
 if (isset($_GET['month'])) {
     $month = $_GET['month'];
@@ -15,7 +10,9 @@ if (isset($_GET['month'])) {
     $month = date("m");
     $years = date("Y");
 }
-
+$specialDate = ["2007-9-30" => "亞絲娜出身", "2008-10-7" => "桐人出身", "2010-10-4" => "莉卡出身","$years-9-30" => "亞絲娜生日", "$years-10-7" => "桐人生日", "$years-10-4" => "莉卡生日", "2011-5-1" => "絕劍有紀生日","$years-5-1" => "絕劍有紀生日", "2020-1-1" => "詩乃-遭遇郵局搶案", "2020-7-15" => "亞絲娜-獨自前往祖父母家", "2022-5-1" => "《Sword Art Online》發表，NERvGear開始販售，桐人成為封測玩家", "2022-8-11" => "桐人進行SAO封閉測試(到達第十層)", "2022-11-6" => "〈SAO篇開始〉", "2022-11-30" => "大斷線事件，全體玩家的身體被移到各地的醫療設施", "2022-12-1" => "死亡人數達到2000人", "2022-12-2" => "桐人與亞絲娜在第一層迷宮初次見面", "2022-12-3" => "蒂爾貝魯發現第一層Boss房間", "2023-2-1" => "亞絲娜加入《血盟騎士團》", "2023-4-8" => "桐人加入月夜的黑貓團", "2023-5-16" => "桐人在下水道找到躲藏的幸", "2023-6-22" => "幸死亡and月夜的黑貓團全滅", "2024-2-23" => "桐人與西莉卡第一次相遇", "2024-10-18" => "桐人星爆氣流斬首次登場", "2024-10-24" => "桐人與亞絲娜線上婚禮", "2024-10-30" => "桐人與亞絲娜發現結衣", "2024-11-7" => "〈SAO篇結束〉", "2025-1-20" => "桐人登入ALO", "2025-11-9" => "死槍開第一槍", "2025-12-7" => "〈GGO篇開始〉", "2025-12-16" => "〈GGO篇結束〉", "2025-12-31" => "絕劍的傳聞開始出現", "2026-1-7" => "絕劍vs亞絲娜", "2026-1-8" => "沉睡騎士立下公會碑", "2026-3-29" => "有紀將原創劍技-聖母聖詠送給亞絲娜"];
+$nowmonth = date("m");
+$nowyesrs = date("Y");
 $lastmonth = $month - 1;
 $nextmonth = $month + 1;
 $lastyears = $years;
@@ -35,12 +32,23 @@ if ($month == 12) {
 }
 
 
+
 // 每個月幾天
 $days = date("t", strtotime("$years-$month-01"));
 $firstday = date("$years-$month-01");
 $week = date("w", strtotime("$years-$month-01"));
 // 第一天的位置
 $first = 1 - $week;
+
+
+// 製作背景
+// for ($i = 1; $i <= 12; $i++) {
+//     $bg = 'bg' . $i;
+
+//     if ($i == $month) {
+//         $bg='background-image: url(./SAO/sao-'.$i.'.jpg)';
+//     }
+// }
 
 ?>
 
@@ -52,102 +60,133 @@ $first = 1 - $week;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>萬年曆</title>
-    <!-- <link rel="stylesheet" href="style.css"> -->
-    <style>
-        .main{
-            background-image: url(./SAO/ymu.jpg);
-        }
-        table,
-        tr,
-        td {
-            border: 1px solid black;
-            text-align: center;
-        }
+    <link rel="stylesheet" href="style.css">
 
-        table {
-            width: 50%;
-            height: 50%;
-
-        }
-
-        * {
-            margin: 0px auto;
-            text-align: center;
-        }
-
-        header>h1 {
-            margin: 0px auto;
-        }
-    </style>
 </head>
 
-<body class="main">
-    <header>
-        <h1><?= $years ?>年<?= $month ?>月</h1>
-    </header>
-    <a href="calendar.php?years=<?= $lastyears; ?>&month=<?= $lastmonth; ?>">上個月</a>
-    <a href="calendar.php?years=<?= $nextyears; ?>&month=<?= $nextmonth; ?>">下個月</a>
+<body style="background:url(SAO/sao-<?= $month ?>.jpg)" class="main">
 
-    <form action="./calendar.php" method="GET">
-        <select name="years" style="font-size:24px;width: 10%;">
-            <?php
-            // echo "<option value='$years' selected>$years</option>";
-            for ($i = 1900; $i <= 2400; $i++) {
-                $nextyears = $i;
-                if ($nextyears == $years) {
-                    echo "<option value='$nextyears' selected>$nextyears</option>";
-                }else{
-                    echo "<option value='$nextyears'>$nextyears</option>";
-                }
-            }
-            ?>
-            <label for="username">年</label>
-        </select>
-        <select name="month" style="font-size:24px;width: 10%;">
-            <?php
-            for ($i = 1; $i <= 12; $i++) {
-                $nextmonth = $i;
-                if ($nextmonth == $month) {
-                    echo "<option value='$nextmonth' selected>$nextmonth</option>";
-                }else{
-                    echo "<option value='$nextmonth'>$nextmonth</option>";
-                }
-            }
-            ?>
-            <label for="username">月</label>
-        </select>
-        <input type="submit" value="submit">
-    </form>
-    <table>
-        <tr>
-            <?php
-            for ($i = 0; $i < 7; $i++) {
-                echo "<td>$weekend[$i]</td>";
-            }
-            ?>
-        </tr>
-        <?php
-        for ($i = $first; $i < $days;) {
-            echo '<tr>';
-            for ($j = 0; $j < 7; $j++) {
-                if ($i <= $days and $i > 0) {
-                    if ($j == 0 or $j == 6) {
-                        echo "<td style='background-color:red;'>{$i}</td>";
-                    } elseif ($j > 0 and $j < 6) {
-                        echo "<td>{$i}</td>";
+    <aside class="aside ">
+        <header class="header">
+            <h1><?= $years ?>年</h1>
+            <h3><?= $month ?>月</h3>
+        </header>
+        <footer class="footer">
+            <form action="./calendar.php" method="GET">
+                <h4>years：</h4>
+                <select name="years" style="font-size:20px;width: 50%;background-color:lightblue">
+                    <?php
+                    // echo "<option value='$years' selected>$years</option>";
+                    for ($i = 1900; $i <= 2400; $i++) {
+                        $seleyears = $i;
+                        if ($seleyears == $years) {
+                            echo "<option value='$seleyears' selected >$seleyears</option>";
+                        } else {
+                            echo "<option value='$seleyears'>$seleyears</option>";
+                        }
                     }
-                } else {
-                    echo "<td>&nbsp;</td>";
-                }
-                $i++;
-            }
-            echo "</tr>";
-        }
 
+                    ?>
+                    <label for="username">年</label>
+                </select>
+                <br>
+
+                <h4>month：</h4>
+                <select name="month" style="font-size:20px;width: 50%;background-color:lightblue">
+                    <?php
+                    for ($i = 1; $i <= 12; $i++) {
+                        $selemonth = $i;
+                        if ($selemonth == $month) {
+                            echo "<option value='$selemonth' selected>$selemonth</option>";
+                        } else {
+                            echo "<option value='$selemonth'>$selemonth</option>";
+                        }
+                    }
+
+                    ?>
+                    
+                    <label for="username">月</label>
+                </select>
+                <br><br>
+                <input type="submit" value="選擇" style="font-size:20px;width: 30%;background-color:lightblue">
+            </form>
+        </footer>
+
+    </aside>
+    <section class="section">
+
+        <header class="header2">
+            <table class="null">
+                <tr>
+                    <td class="null1"><a href="calendar.php?years=<?= $lastyears; ?>&month=<?= $lastmonth; ?>">上個月</a></td>
+                    <td class="null1"><a href="calendar.php?years=<?= $nowyesrs; ?>&month=<?= $nowmonth; ?>">當月</a></td>
+                    <td class="null1"><a href="calendar.php?years=<?= $nextyears; ?>&month=<?= $nextmonth; ?>">下個月</a></td>
+
+                </tr>
+            </table>
+            <hr>
+        </header>
+        
+        <div>
+            <table class="table">
+                <tr>
+                    <?php
+                    for ($i = 0; $i < 7; $i++) {
+                        echo "<td class='tdweek'>$weekend[$i]</td>";
+                    }
+                    ?>
+                </tr>
+
+                <?php
+
+                for ($i = $first; $i < $days;) {
+                    echo '<tr>';
+                    for ($j = 0; $j < 7; $j++) {
+                        $date = date("$years-$month-$i");
+                        if ($i <= $days and $i > 0) {
+                            if (($j == 0 or $j == 6) and !(isset($date) and array_key_exists($date, $specialDate))) {
+                                echo "<td class='tdweeks'>{$i}</td>";
+                            } 
+                            if (($j > 0 and $j < 6) and !(isset($date) and array_key_exists($date, $specialDate))) {
+                                echo "<td class='tdday'>{$i}</td>";
+                            }
+                            if (isset($date) and array_key_exists($date, $specialDate)) {
+                                echo "<td class='speciaDay'>$i <p>$specialDate[$date]</p></td>";
+                            }
+                        } else {
+                            echo "<td>&nbsp;</td>";
+                        }
+                        $i++;
+                    }
+                    echo "</tr>";
+                }
+
+
+                ?>
+            </table>
+        </div>
+<br><br>
+        <?php
+        $quotations = [
+            '',
+            '一個好的工程師是那種過單行道馬路都要左顧右盼的人。',
+            '程式有問題時不要擔心。如果所有東西都沒問題，你就失業',
+            '大部分人都知道通常一名工程師會具有的美德。當然了，有三種：懶惰、暴躁、傲慢。',
+            '寫程式時要保持這種心態：就好像將來要維護你這些程式的人是一位殘暴的精神病患者，而且他知道你住在哪。',
+            '一個人寫的爛軟體將會給另一個人帶來一份全職工作。',
+            '沒能一次成功，那就叫它 1.0 版吧。',
+            '所有的工程師都是編劇，所有的電腦都是爛演員。',
+            '傻瓜都能寫出電腦能理解的程式。優秀的工程師寫出的是人類能讀懂的程式。',
+            '開發軟體和建造教堂非常相似——完工之後我們就開始祈禱。',
+            '世上只有兩類程式語言：那些被人罵翻的，以及那些沒人使用的。',
+            'Java 之於 JavaScript 的關係，就如同狗和熱狗。',
+            '電腦善於遵循指令，但不善於理解你的思維。'
+        ];
+
+        echo "<h1 class='h1'>$quotations[$month]</h1>";
 
         ?>
-    </table>
-
+    </section>
 </body>
 
 </html>
