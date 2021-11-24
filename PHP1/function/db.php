d9e335a1b1cd715d6bb476c9bb3cd920523fdc5d
tommy722 <t40201@gmail.com>
2021-11-24 16:36:13 +0800

    function

--- PHP1/function/db.php
+++ PHP1/function/db.php
@@ -1,19 +1,25 @@
 <?php
     $dsn="mysql:host=localhost;charset=utf8;dbname=students";
     $pdo=new PDO($dsn,'root','');
-
-  
     function find($table,$id){
         global $pdo;
         $sql="SELECT * FROM `$table` WHERE `id`='$id'";
     
         return $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
-
     //取出指定資料表的所有資料
-function all($table){
+function all($table,...$arg){
     global $pdo;
     $sql="SELECT * FROM `$table`";
+    if(isset($arg[0])){
+        if(is_array($arg[0])){
+            $sqk=$sql.$arg[0];
+        }else{
+            $sqk=$sql.$arg[0];
+        }
+    }
+
+    
     $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     return $rows;
     //return $pdo->query($sql)->fetchAll();
@@ -27,7 +33,7 @@ function all($table){
  * 
  */
 $column=['date'=>'2021-11-22',
-         'place'=>'職訓中心'];
+        'place'=>'職訓中心'];
 $where=['payment_method'=>'信用卡',
         'classification'=>'交通'];
         
@@ -40,30 +46,27 @@ foreach ($array as $key => $value) {
 
 }
 echo "迴圈完成後=>".mb_substr($sql,0,mb_strlen($sql)-5); */
- function update($table,$column,$where){
+function update($table,$column,$where){
     global $pdo;
-
     $sql_set='';
     foreach ($column as $key => $value) {
         $sql_set=$sql_set . "`$key`='$value',";
     }
     $sql_set=trim($sql_set,',');
-
     $sql_where='';
     foreach ($where as $key => $value) {
         $sql_where=$sql_where . "`$key`='$value' AND ";
     }
     $sql_where=mb_substr($sql_where,0,mb_strlen($sql_where)-5);;
-
     mb_substr($sql_where,0,mb_strlen($sql_where)-5);
     $sql="UPDATE `$table` SET $sql_set WHERE $sql_where ";
     "UPDATE `expenditure` 
         SET `date`='2021-11-22',`place`='泰山訓練場' 
-         WHERE `payment_method`='信用卡' AND `classification`='教育'";
+        WHERE `payment_method`='信用卡' AND `classification`='教育'";
     //echo $sql;
     $pdo->exec($sql);
 
- }
+}
 
 
 ?>
